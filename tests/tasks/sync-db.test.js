116ce9720b5d7ffee06507126dbd4be0c435d2f6
Axel <chavarriamontoyaaxel01@gmail.com>
2025-04-28 22:02:49 -0600

    Cambiando mensaje de testing

--- tests/tasks/sync-db.test.js
+++ tests/tasks/sync-db.test.js
@@ -1,7 +1,7 @@
 const { syncDb } = require("../../tasks/sync-db")
 
 describe('Pruebas en sync-db', () => {
-    test('Debe ejecutar el proceso 2 veces', () => {
+    test('TEST: Debe ejecutar el proceso 2 veces', () => {
         syncDb();
         const times = syncDb()
         expect(times).toBe(2)
